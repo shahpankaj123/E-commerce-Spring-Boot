469707463f7e93e9aedd99caec06df140a20713b
Aaryan Shah <aaryanshah615@gmail.com>
2024-08-31 17:33:38 +0545

    product added

--- src/main/java/com/pankaj/ecommerce/Models/Category.java
+++ src/main/java/com/pankaj/ecommerce/Models/Category.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private UUID categoryId;
 
     private String name;
 
---
+++ src/main/java/com/pankaj/ecommerce/Models/Product.java
@@ -0,0 +1,38 @@
+package com.pankaj.ecommerce.Models;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private int quantity;
+
+    @ManyToOne 
+    @JoinColumn(name = "categoryId") 
+    private Category category;
+
+    @Lob
+    private byte[] image;
+    
+}
